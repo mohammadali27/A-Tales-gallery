00e1f69882d2fffdaba234604a8e0e0f2634c844
mohammadali27 <amohammad6627@gmail.com>
2026-05-04 02:11:36 +0600

    add all

--- src/app/Allphotos/page.jsx
+++ src/app/Allphotos/page.jsx
@@ -1,39 +1,47 @@
 import Image from "next/image";
 import { Card } from "@heroui/react";
 import Link from "next/link";
+import Catagory from "@/components/Catagory";
 
-const AllPhotosCard = async () => {
+const AllPhotosCard = async ({ searchParams }) => {
+  const {category} = await searchParams?.category || "";
   const res = await fetch("https://a-tales-gallery.vercel.app/data.json");
   const photos = await res.json();
+  const filteredPhotos =category ? photos.filter(
+    (photo) => photo.category.toLowerCase() == category.toLowerCase(),
+  ) : photos;
 
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  mt-10 mb-5 container mx-auto gap-5">
-      {photos.map((item) => (
-        <Card key={item.image} className="border rounded-xl shadow-lg p-5">
-          <div className=" relative w-full aspect-square">
-            {" "}
-            <Image
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className="rounded-lg"
-              src={item.image}
-              alt={item.title}
-              priority
-            />
-          </div>
-          <div className=" flex items-center justify-between mt-3 mb-5">
-            <p>{item.title}</p>
-            <p>prise $ {item.price}</p>
-          </div>
-          <Link href={`/Allphotos/${item.id}`}>
-            <button className="w-full outline outline-1 outline-gray-400 rounded py-2 px-4">
-              View Details
-            </button>
-          </Link>
-        </Card>
-      ))}
-      ;
-    </div>
+    <>
+      <h1 className=" font-bold text-2xl">All Tiles</h1>
+      <Catagory />
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  mt-10 mb-5 container mx-auto gap-5">
+        {filteredPhotos.map((item) => (
+          <Card key={item.image} className="border rounded-xl shadow-lg p-5">
+            <div className=" relative w-full aspect-square">
+              {" "}
+              <Image
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className="rounded-lg"
+                src={item.image}
+                alt={item.title}
+                priority
+              />
+            </div>
+            <div className=" flex items-center justify-between mt-3 mb-5">
+              <p>{item.title}</p>
+              <p>prise $ {item.price}</p>
+            </div>
+            <Link href={`/Allphotos/${item.id}`}>
+              <button className="w-full outline outline-1 outline-gray-400 rounded py-2 px-4">
+                View Details
+              </button>
+            </Link>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/components/Catagory.jsx
+++ src/components/Catagory.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import { Button } from "@heroui/react";
+import Link from "next/link";
+import React from "react";
 
 const Catagory = async () => {
-    const res = await fetch("https://a-tales-gallery.vercel.app/data.json")
-    const catagorys = await res.json();
-    return (
-        <div>
-            
-        </div>
-    );
+  const res = await fetch("https://a-tales-gallery.vercel.app/data.json");
+  const catagorys = await res.json();
+
+  return (
+    <div className=" space-x-3 space-y-3 mt-5 gap-3">
+      {catagorys.map((cat) => (
+        <Link key={cat.id} href={`?catagory=${cat.title.toLowerCase()}`}>
+          <Button variant="outline" size="sm">
+            <p>{cat.title}</p>
+          </Button>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Catagory;
